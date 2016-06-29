5957a48c9339d9297581dcc260157c87136179ae
Jack-Lee-Hiter <jack.lee@outlook.com>
2016-06-29 21:39:48 +0800

    infix2postfix

--- Stack.py
+++ Stack.py
@@ -111,3 +111,39 @@ def baseConverter(decNumber, base):
     return newString
 
 print(baseConverter(59, 16))
+
+# 利用栈实现普通多项式的后缀表达式
+def infixToPostfix(infixexpr):
+    prec = {}
+    prec['*'] = 3
+    prec['/'] = 3
+    prec['+'] = 2
+    prec['-'] = 2
+    prec['('] = 1
+    opStack = Stack()
+    postfixList = []
+    tokenList = infixexpr.split()
+
+    for token in tokenList:
+        if token in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' or token in '0123456789':
+            postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            topToken = opStack.pop()
+            while topToken != '(':
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and (prec[opStack.peek()] >= prec[token]):
+                postfixList.append(opStack.pop())
+            opStack.push(token)
+
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+
+    return ''.join(postfixList)
+
+print(infixToPostfix("A * B + C * D"))
+print(infixToPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+
