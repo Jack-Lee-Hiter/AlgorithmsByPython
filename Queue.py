74ffd863ffb29fdcdf6f8c1cb4650bd8bf341234
Jack-Lee-Hiter <jack.lee@outlook.com>
2016-07-01 12:29:51 +0800

    build queue

---
+++ Queue.py
@@ -0,0 +1,78 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+q = Queue()
+q.enqueue(4)
+q.enqueue('god')
+q.enqueue(True)
+print(q.dequeue())
+
+def hotPotato(namelist, num):
+    simqueue = Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+
+    while simqueue.size() > 1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+        simqueue.dequeue()
+
+    return simqueue.dequeue()
+
+print(hotPotato(["Bill","David","Susan","Jane","Kent","Brad"],7))
+
+# 设计一个良策都能插入删除的队列deque
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+# 利用Deque解决回文字符串
+def palcheker(aString):
+    chardeque = Deque()
+
+    for ch in aString:
+        chardeque.addFront(ch)
+
+    stillEqual = True
+
+    while chardeque.size() > 1 and stillEqual:
+        first = chardeque.removeFront()
+        last = chardeque.removeRear()
+        if first != last:
+            stillEqual = False
+
+    return stillEqual
+
+print(palcheker('lsdkjfskf'))
+print(palcheker('radar'))
