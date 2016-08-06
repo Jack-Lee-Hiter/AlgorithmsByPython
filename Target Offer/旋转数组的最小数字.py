bca62bd0989536d43f496697c3e6b635a2a20f3d
Jack-Lee-Hiter <jack.lee@outlook.com>
2016-08-06 22:28:58 +0800

    add sth

--- Target Offer/旋转数组的最小数字.py
+++ Target Offer/旋转数组的最小数字.py
@@ -29,6 +29,31 @@ class Solution:
                             rear = i
             minVal = rotateArray[rear]
             return minVal
+    # 书上方法
+    def minNumberInRotateArray2(self, rotateArray):
+        if len(rotateArray) == 0:
+            return 0
+        front, rear = 0, len(rotateArray) - 1
+        midIndex = 0
+        while rotateArray[front] >= rotateArray[rear]:
+            if rear - front == 1:
+                midIndex = rear
+                break
+            midIndex = (front + rear) // 2
+            if rotateArray[front] == rotateArray[rear] and rotateArray[front] == rotateArray[midIndex]:
+                return self.MinInOrder(rotateArray, front, rear)
+
+            if rotateArray[midIndex] >= rotateArray[front]:
+                front = midIndex
+            elif rotateArray[midIndex] <= rotateArray[rear]:
+                rear = midIndex
+        return rotateArray[midIndex]
+    def MinInOrder(self, array, front, end):
+        result = array[0]
+        for i in array[front:end+1]:
+            if i < result:
+                result = i
+        return result
 
 Test = Solution()
 print(Test.minNumberInRotateArray([3, 4, 5, 1, 2]))
