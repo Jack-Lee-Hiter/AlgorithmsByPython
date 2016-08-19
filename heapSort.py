a667d8294918c5a93f1d9f5dff8016bbed25553f
Jack.ht.lee <Jack-Lee-Hiter@users.noreply.github.com>
2016-08-19 22:34:37 +0800

    fix bug

--- heapSort.py
+++ heapSort.py
@@ -5,22 +5,21 @@ def heapSort(alist):
     output = []
     for i in range(length):
         tempLen = len(alist)
-        for j in range((tempLen-1)//2, -1, -1):
-            k = j
-            v, heap = alist[k], False
-            while not heap and 2*k <= tempLen-1:
-                index = 2 * k
-                if index < tempLen-1:
-                    if alist[index] < alist[index+1]:
-                        index += 1
-                if v >= alist[index]:
+        for j in range(tempLen//2-1, -1, -1):
+            preIndex = j
+            preVal, heap = alist[preIndex], False
+            while 2 * preIndex <= tempLen - 1 and not heap:
+                curIndex = 2 * preIndex + 1
+                if curIndex < tempLen - 1:
+                    if alist[curIndex] < alist[curIndex+1]:
+                        curIndex += 1
+                if preVal >= alist[curIndex]:
                     heap = True
                 else:
-                    alist[k] = alist[index]
-                    k = index
-            alist[k] = v
-        heapVal = alist.pop(0)
-        output.insert(0, heapVal)
+                    alist[preIndex] = alist[curIndex]
+                    preIndex = curIndex
+            alist[preIndex] = preVal
+        output.insert(0, alist.pop(0))
     return output
 
 test = [2, 6, 5, 9, 10, 3, 7]
